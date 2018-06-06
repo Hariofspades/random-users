1fb9a15fbf918b0811959eaf95606ddb1ca52c8b
Hari <hariutd@gmail.com>
2018-06-06 20:37:40 +0530

    repository implementation

---
+++ data/src/main/java/com/hariofspades/data/features/userlist/UserDataRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package com.hariofspades.data.features.userlist
+
+import com.hariofspades.data.features.userlist.mapper.ResultItemMapper
+import com.hariofspades.data.features.userlist.repository.UserRemote
+import com.hariofspades.domain.features.userlist.UserDataRepository
+import com.hariofspades.domain.features.userlist.model.ResultsItem
+import io.reactivex.Single
+
+class UserDataRepositoryImpl(
+
+        private val resultItemMapper: ResultItemMapper,
+        private val userRemote: UserRemote
+
+) : UserDataRepository {
+
+    override fun getRandomUserList(page: Int): Single<List<ResultsItem>> {
+        return userRemote.getRandomUserList(page)
+                .map { it.map(resultItemMapper::mapFromEntity) }
+    }
+
+}
---
+++ domain/src/main/java/com/hariofspades/domain/features/userlist/UserDataRepository.kt
@@ -0,0 +1,9 @@
+package com.hariofspades.domain.features.userlist
+
+import com.hariofspades.domain.features.userlist.model.ResultsItem
+import io.reactivex.Single
+
+interface UserDataRepository {
+
+    fun getRandomUserList(page: Int): Single<List<ResultsItem>>
+}
